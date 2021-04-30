3b03522e42a092f51de2b6eff3f629e2462af41d
Jeffrey Hurchalla <hurchalla@yahoo.com>
2021-04-29 22:25:29 -0700

    add tests for is_prime_trial_division.h.  Simplify and make minor fixes to the primality code

---
+++ test/detail_tests/test_is_prime_trialdivision.cpp
@@ -0,0 +1,154 @@
+// --- This file is distributed under the MIT Open Source License, as detailed
+// by the file "LICENSE.TXT" in the root of this repository ---
+// Author: Jeffrey Hurchalla
+
+#include "hurchalla/factoring/detail/PrimeTrialDivisionWarren.h"
+#include "hurchalla/factoring/detail/PrimeTrialDivisionMayer.h"
+#include "hurchalla/factoring/detail/SieveOfEratosthenes.h"
+#include "hurchalla/factoring/detail/is_prime_miller_rabin.h"
+#include "hurchalla/factoring/detail/is_prime_trialdivision.h"
+#include "hurchalla/util/traits/ut_numeric_limits.h"
+#include "hurchalla/util/compiler_macros.h"
+#include <cstdint>
+
+#include "gtest/gtest.h"
+
+namespace {
+
+
+using namespace hurchalla;
+using namespace hurchalla::detail;
+
+
+template <typename T>
+bool get_primality(T x)
+{
+    if (x < 2)
+        return false;
+    if (x % 2 == 0)
+        return x == 2;
+    return is_prime_miller_rabin_integral(x);
+}
+
+
+template <int SIZE, typename T>
+void iptd_test(T x, int size_limit, const std::vector<std::uint64_t>& primevec)
+{
+    if (size_limit > SIZE)
+        size_limit = SIZE;
+    using size_type = std::vector<std::uint64_t>::size_type;
+    // a failure here would mean this test is buggy
+    static_assert(ut_numeric_limits<int>::digits
+                  <= ut_numeric_limits<size_type>::digits);
+    std::uint64_t nextprime = primevec[static_cast<size_type>(size_limit)];
+    // These tests need for nextprime*nextprime to still fit in a uint64_t.
+    // The tests would be at fault if this is false, but mostly just for
+    // choosing a SIZE that's way larger than needed.
+    EXPECT_TRUE(nextprime < static_cast<std::uint64_t>(1)<<32);
+
+    bool success;
+    bool isprime = is_prime_trialdivision<PrimeTrialDivisionMayer, SIZE>(
+                                                        x, success, size_limit);
+    if (x < nextprime*nextprime) {
+        EXPECT_TRUE(success);
+    }
+    if (success) {
+        EXPECT_TRUE(isprime == get_primality(x));
+    }
+
+    isprime = is_prime_trialdivision<PrimeTrialDivisionWarren, SIZE>(
+                                                        x, success, size_limit);
+    if (x < nextprime*nextprime) {
+        EXPECT_TRUE(success);
+    }
+    if (success) {
+        EXPECT_TRUE(isprime == get_primality(x));
+    }
+}
+
+
+template <int SIZE, typename T>
+void iptd_sized_tests(const SieveOfEratosthenes& sieve)
+{
+    static_assert(1 < SIZE);
+    static_assert(!std::numeric_limits<T>::is_signed);
+
+    std::vector<std::uint64_t> primevec;
+    primevec.push_back(2);
+    for (std::uint64_t i=3; ; i+=2) {
+        EXPECT_TRUE(i < sieve.size());  // false would mean this test is buggy
+        if (sieve[i]) {
+            primevec.push_back(i);
+            if (primevec.size() > SIZE)
+                break;
+        }
+    }
+    using size_type = std::vector<std::uint64_t>::size_type;
+    // a failure here would mean this test is buggy
+    static_assert(ut_numeric_limits<int>::digits
+                  <= ut_numeric_limits<size_type>::digits);
+
+    T midpoint = (static_cast<T>(0) - 1)/2;
+    T midpoint_minus50 = static_cast<T>(midpoint - 50);
+    T max = static_cast<T>(static_cast<T>(0) - 1);
+
+    std::vector<int> limits = { 1, 2, 5, SIZE, SIZE-1, SIZE/2, SIZE/2 + 1 };
+    for (int size_limit : limits) {
+        for (T x = 0; x < 255; ++x)
+            iptd_test<SIZE>(x, size_limit, primevec);
+        for (T x = max; x > max - 100; --x)
+            iptd_test<SIZE>(x, size_limit, primevec);
+        for (T x = midpoint_minus50; x < midpoint + 50; ++x)
+            iptd_test<SIZE>(x, size_limit, primevec);
+        std::uint64_t prime = primevec[static_cast<size_type>(size_limit)];
+        iptd_test<SIZE>(static_cast<T>(prime-2), size_limit, primevec);
+        iptd_test<SIZE>(static_cast<T>(prime-1), size_limit, primevec);
+        iptd_test<SIZE>(static_cast<T>(prime+0), size_limit, primevec);
+        iptd_test<SIZE>(static_cast<T>(prime+1), size_limit, primevec);
+        iptd_test<SIZE>(static_cast<T>(prime+2), size_limit, primevec);
+    }
+}
+
+
+template <typename T>
+void iptd_typed_tests(const SieveOfEratosthenes& sieve)
+{
+    iptd_sized_tests<2,T>(sieve);
+    iptd_sized_tests<10,T>(sieve);
+    iptd_sized_tests<54,T>(sieve);
+    if constexpr (ut_numeric_limits<T>::digits >= 16) {
+        iptd_sized_tests<55,T>(sieve);
+        iptd_sized_tests<198,T>(sieve);
+        iptd_sized_tests<1000,T>(sieve);
+    }
+    // Using SIZE as large as 3501 (and even 1000 above) faces some risk of
+    // getting a compiler failure of
+    // "constexpr evaluation hit maximum step limit; possible infinite loop?"
+    // It's not a bug in any of the code under test or the test itself, it's
+    // just a limit of the compiler with constexpr initialization.  There's
+    // probably a compiler flag you can set to increase the limit, but if
+    // you run into this error I'd simply suggest commenting out the test 
+    // here that caused it - even the test with SIZE of 1000 is far larger
+    // than I expect we'd ever use in practice for trial division.  I'd only
+    // try to fix such an error if it happens with SIZE < 200, since that's
+    // a more practical range (though ~200 is still fairly large).
+    if constexpr (ut_numeric_limits<T>::digits >= 32) {
+        iptd_sized_tests<3501,T>(sieve);
+    }
+}
+
+
+TEST(HurchallaFactoringIsPrimeTrialDivision, iptd_tests) {
+    SieveOfEratosthenes sieve(static_cast<std::uint64_t>(1)<<24);
+
+    iptd_typed_tests<std::uint8_t>(sieve);
+    iptd_typed_tests<std::uint16_t>(sieve);
+    iptd_typed_tests<std::uint32_t>(sieve);
+    iptd_typed_tests<std::uint64_t>(sieve);
+#if HURCHALLA_COMPILER_HAS_UINT128_T()
+    iptd_typed_tests<__uint128_t>(sieve);
+#endif
+}
+
+
+} // end namespace
